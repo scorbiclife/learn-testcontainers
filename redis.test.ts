8d7116afb814d6831b17ebc66461f3a00098e28f
scorbiclife <scorbiclife+git@gmail.com>
2026-01-17 16:58:33 +0900

    refactor: use boilerplate for redis container

--- redis.test.ts
+++ redis.test.ts
@@ -1,16 +1,16 @@
 import { describe, it, beforeAll, afterAll } from "@jest/globals";
-import { createClient, RedisClientType } from "redis";
-import { GenericContainer, StartedTestContainer } from "testcontainers";
+import { createClient, type RedisClientType } from "redis";
+import { RedisContainer, type StartedRedisContainer } from "@testcontainers/redis";
 
 describe("Redis", () => {
-    let container: StartedTestContainer;
+    let container: StartedRedisContainer;
     let redisClient: RedisClientType;
 
     beforeAll(async () => {
-        container = await new GenericContainer("redis:8")
+        container = await new RedisContainer("redis:8")
             .withExposedPorts(6379)
             .start();
-        const redisUrl = `redis://${container.getHost()}:${container.getMappedPort(6379)}`;
+        const redisUrl = container.getConnectionUrl();
         redisClient = createClient({ url: redisUrl });
         await redisClient.connect();
     });
